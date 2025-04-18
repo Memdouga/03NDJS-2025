ba4aeff140afe400775451b081eb6aeb99a0befa
memdouga <mengue7745@gmail.com>
2025-04-18 10:16:53 +0200

    Ajouter la constante mongoDB et recommencer

--- Day_4/server.js
+++ Day_4/server.js
@@ -1,23 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-const mongoose = require('mongoose');
+
 const app = express();
-gi
-// Middleware
-app.use(cors());
-app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-
-// Test route
-app.get('/', (req, res) => {
-  res.send('Auth API');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/authDB', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));

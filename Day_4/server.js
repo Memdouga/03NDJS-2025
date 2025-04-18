e6618fcef9a053e758b60e1175c6d61bc70164bc
memdouga <mengue7745@gmail.com>
2025-04-18 10:22:39 +0200

    J'ai essayer de mettre la base de donnees sur mongoDB avec le code

--- Day_4/server.js
+++ Day_4/server.js
@@ -14,3 +14,10 @@ mongoose.connect('mongodb://localhost:27017/authDB', {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);

5fe45762ae4fb2e97bcfe0be832f1bdb98b59f78
memdouga <mengue7745@gmail.com>
2025-04-18 10:43:05 +0200

    Dans authController.js ; ajout de la constante register

--- Day_4/ExamenNodeJs/controllers/authController.js
+++ Day_4/ExamenNodeJs/controllers/authController.js
@@ -1,3 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;

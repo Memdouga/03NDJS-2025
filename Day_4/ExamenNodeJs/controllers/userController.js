91528de7ddfd1217efed2774fc4d7da29be9322f
memdouga <mengue7745@gmail.com>
2025-04-18 11:59:49 +0200

    updated usercontroller.js

--- Day_4/ExamenNodeJs/controllers/userController.js
+++ Day_4/ExamenNodeJs/controllers/userController.js
@@ -2,25 +2,27 @@ const User = require('../models/User');
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = User.getAll();
+    const users = await User.find().select('-password');
     res.json(users);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: 'Server error' });
   }
 };
 
 const deleteUser = async (req, res) => {
   try {
-    const id = parseInt(req.params.id);
-    const user = User.delete(id);
-
+    const user = await User.findByIdAndDelete(req.params.id);
+    
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
     res.json({ message: 'User deleted successfully' });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    if (error.kind === 'ObjectId') {
+      return res.status(400).json({ error: 'Invalid user ID' });
+    }
+    res.status(500).json({ error: 'Server error' });
   }
 };
 

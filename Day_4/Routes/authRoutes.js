10c557c0fe95c445282653d2d513956b08c93c29
memdouga <mengue7745@gmail.com>
2025-04-18 11:28:33 +0200

    S'il ya pas de Token

--- Day_4/Routes/authRoutes.js
+++ Day_4/Routes/authRoutes.js
@@ -5,3 +5,7 @@ const auth = async (req, res, next) => {
     try {
       // Get token from header
       const token = req.header('Authorization')?.replace('Bearer ', '');
+
+      if (!token) {
+        return res.status(401).json({ error: 'No token, authorization denied' });
+      }

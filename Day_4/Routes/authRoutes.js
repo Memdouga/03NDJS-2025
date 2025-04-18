4358f05291f582ca4015e77fe3d8b950858e8d36
memdouga <mengue7745@gmail.com>
2025-04-18 11:26:43 +0200

    Creatio des constantes JWT et User

--- Day_4/Routes/authRoutes.js
+++ Day_4/Routes/authRoutes.js
@@ -1,10 +1,2 @@
-const express = require('express');
-const { register, login, getMe } = require('../ExamenNodeJs/controllers/authController');
-
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', getMe);
-
-module.exports = router;
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');

5c8c90d44942ce155a27e38976b1702edc7a1655
memdouga <mengue7745@gmail.com>
2025-04-18 11:27:49 +0200

    creation de la const auth

--- Day_4/Routes/authRoutes.js
+++ Day_4/Routes/authRoutes.js
@@ -1,2 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const auth = async (req, res, next) => {
+    try {
+      // Get token from header
+      const token = req.header('Authorization')?.replace('Bearer ', '');
